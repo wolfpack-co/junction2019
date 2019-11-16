3ceec9b379ed7137475824dbcf73f76c3c20478d
mmitropolitsky <milkomitropolitsky@gmail.com>
2019-11-16 15:54:04 +0100

    swagger

--- src/main/java/com/wolfpack/vridgeapi/VridgeApiApplication.java
+++ src/main/java/com/wolfpack/vridgeapi/VridgeApiApplication.java
@@ -1,19 +1,34 @@
 package com.wolfpack.vridgeapi;
 
 import com.wolfpack.vridgeapi.config.WebConfig;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@Import(WebConfig.class)
+@Import({ WebConfig.class })
 @EntityScan("com.wolfpack.vridgeapi.model")
+@EnableSwagger2
 public class VridgeApiApplication {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VridgeApiApplication.class, args);
--- src/main/java/com/wolfpack/vridgeapi/endpoint/ProductContoller.java
+++ src/main/java/com/wolfpack/vridgeapi/endpoint/ProductContoller.java
@@ -6,9 +6,12 @@ import com.wolfpack.vridgeapi.model.Product;
 import com.wolfpack.vridgeapi.service.ProductService;
 import org.apache.catalina.connector.Response;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +38,44 @@ public class ProductContoller {
 			return ResponseEntity.ok().build();
 		}
 		catch (Exception e) {
-			return ResponseEntity.status(Response.SC_INTERNAL_SERVER_ERROR).build();
+			return ResponseEntity.status(Response.SC_INTERNAL_SERVER_ERROR).body(e.getMessage());
 		}
 	}
+
+	@PutMapping("/{productId}/book")
+	public ResponseEntity bookProduct(@PathVariable("productId") int id, @RequestBody Product product) {
+		try {
+			// TODO set creator on product
+			productService.bookProduct(product);
+			return ResponseEntity.ok().build();
+		}
+		catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}
+	}
+
+	@PutMapping("/{productId}/confirm")
+	public ResponseEntity confirmProductBooking(@PathVariable("productId") int id, @RequestBody Product product) {
+		try {
+			// TODO set creator on product
+			productService.confirmBooking(id);
+			return ResponseEntity.ok().build();
+		}
+		catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}
+	}
+
+	@PutMapping("/{productId}/complete")
+	public ResponseEntity completeProductBooking(@PathVariable("productId") int id, @RequestBody Product product) {
+		try {
+			// TODO set creator on product
+			productService.completeBooking(id);
+			return ResponseEntity.ok().build();
+		}
+		catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}
+	}
+
 }
